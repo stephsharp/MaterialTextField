71ff7d6ff386bd3ba80ef1e676be313002dc611c
Stephanie Sharp <ssharp@odecee.com.au>
2015-07-22 17:37:49 +1000

    Add UIColor category with default form colors

---
+++ MaterialFormKit/UIColor+MaterialFormKit.h
@@ -0,0 +1,20 @@
+//
+//  UIColor+MaterialFormKit.h
+//  MaterialFormKit
+//
+//  Created by Steph Sharp on 22/07/2015.
+//  Copyright (c) 2015 Stephanie Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (MaterialFormKit)
+
++ (UIColor *)mf_veryDarkGrayColor;
++ (UIColor *)mf_darkGrayColor;
++ (UIColor *)mf_midGrayColor;
++ (UIColor *)mf_lightGrayColor;
++ (UIColor *)mf_greenColor;
++ (UIColor *)mf_redColor;
+
+@end
---
+++ MaterialFormKit/UIColor+MaterialFormKit.m
@@ -0,0 +1,51 @@
+//
+//  UIColor+MaterialFormKit.m
+//  MaterialFormKit
+//
+//  Created by Steph Sharp on 22/07/2015.
+//  Copyright (c) 2015 Stephanie Sharp. All rights reserved.
+//
+
+#import "UIColor+MaterialFormKit.h"
+
+@implementation UIColor (MaterialFormKit)
+
++ (UIColor *)colorWithHex:(int)hex
+{
+    return [UIColor colorWithRed:((CGFloat)((hex & 0xFF0000) >> 16)) / 255.0f
+                           green:((CGFloat)((hex & 0x00FF00) >> 8)) / 255.0f
+                            blue:((CGFloat)((hex & 0x0000FF) >> 0)) / 255.0f
+                           alpha:1.0];
+}
+
++ (UIColor *)mf_veryDarkGrayColor
+{
+    return [UIColor colorWithHex:0x222222];
+}
+
++ (UIColor *)mf_darkGrayColor
+{
+    return [UIColor colorWithHex:0x666666];
+}
+
++ (UIColor *)mf_midGrayColor
+{
+    return [UIColor colorWithHex:0x999999];
+}
+
++ (UIColor *)mf_lightGrayColor
+{
+    return [UIColor colorWithHex:0xBBBBBB];
+}
+
++ (UIColor *)mf_greenColor
+{
+    return [UIColor colorWithHex:0x29A39C];
+}
+
++ (UIColor *)mf_redColor
+{
+    return [UIColor colorWithHex:0xED1C29];
+}
+
+@end
