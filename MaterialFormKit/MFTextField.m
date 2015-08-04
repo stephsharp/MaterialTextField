0c7b0a3133fb256013d0aa4fe76002c217db169c
Stephanie Sharp <ssharp@odecee.com.au>
2015-08-04 15:47:36 +1000

    Designable textfield should not leave space for the error label in the storyboard

--- MaterialFormKit/MFTextField.m
+++ MaterialFormKit/MFTextField.m
@@ -639,6 +639,7 @@ static NSTimeInterval const MFDefaultAnimationDuration = 0.3;
     [self setupUnderline];
 
     self.shouldAnimatePlaceholder = NO;
+    [self.errorLabel removeFromSuperview];
 }
 
 @end
