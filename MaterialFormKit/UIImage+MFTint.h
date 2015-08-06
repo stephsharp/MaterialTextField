04ff14a3866cc201c637878f585347764d4f5a62
Stephanie Sharp <ssharp@odecee.com.au>
2015-08-06 15:37:59 +1000

    Add UIImage+MFTint category to project
    See: https://robots.thoughtbot.com/designing-for-ios-blending-modes

---
+++ MaterialFormKit/UIImage+MFTint.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+MFTint.h
+//  MaterialFormKitDemo
+//
+//  Created by Steph Sharp on 6/08/2015.
+//  Copyright (c) 2015 Stephanie Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (MFTint)
+
+- (UIImage *)tintedImageWithColor:(UIColor *)tintColor;
+
+@end
---
+++ MaterialFormKit/UIImage+MFTint.m
@@ -0,0 +1,37 @@
+//
+//  UIImage+MFTint.m
+//  MaterialFormKitDemo
+//
+//  Created by Steph Sharp on 6/08/2015.
+//  Copyright (c) 2015 Stephanie Sharp. All rights reserved.
+//
+
+#import "UIImage+MFTint.h"
+
+@implementation UIImage (MFTint)
+
+- (UIImage *)tintedImageWithColor:(UIColor *)tintColor
+{
+    return [self tintedImageWithColor:tintColor blendingMode:kCGBlendModeDestinationIn];
+}
+
+#pragma mark - Private methods
+
+- (UIImage *)tintedImageWithColor:(UIColor *)tintColor blendingMode:(CGBlendMode)blendMode
+{
+    UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0f);
+    [tintColor setFill];
+    CGRect bounds = CGRectMake(0, 0, self.size.width, self.size.height);
+    UIRectFill(bounds);
+    [self drawInRect:bounds blendMode:blendMode alpha:1.0f];
+
+    if (blendMode != kCGBlendModeDestinationIn)
+        [self drawInRect:bounds blendMode:kCGBlendModeDestinationIn alpha:1.0];
+
+    UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+
+    return tintedImage;
+}
+
+@end
