86f7dbc36e7176e9a541edb0f06a99b274850feb
Adam Sharp <adsharp@me.com>
2015-11-06 06:36:56 +1100

    Add a MaterialFormKit framework project

---
+++ MaterialFormKit/MaterialFormKit.h
@@ -0,0 +1,19 @@
+//
+//  MaterialFormKit.h
+//  MaterialFormKit
+//
+//  Created by Adam Sharp on 6/11/2015.
+//  Copyright © 2015 Stephanie Sharp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MaterialFormKit.
+FOUNDATION_EXPORT double MaterialFormKitVersionNumber;
+
+//! Project version string for MaterialFormKit.
+FOUNDATION_EXPORT const unsigned char MaterialFormKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MaterialFormKit/PublicHeader.h>
+
+
