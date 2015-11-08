373a0a4d7a82768ae1e01998a377388e9bd5db97
Stephanie Sharp <ssharp@odecee.com.au>
2015-11-09 10:44:18 +1100

    Tidy up UIImage+MFTint category

--- MaterialTextField/UIImage+MFTint.h
+++ MaterialTextField/UIImage+MFTint.h
@@ -5,6 +5,9 @@
 //  Created by Steph Sharp on 6/08/2015.
 //  Copyright (c) 2015 Stephanie Sharp. All rights reserved.
 //
+//  With thanks to Gordon Fontenot from thoughtbot:
+//  https://robots.thoughtbot.com/designing-for-ios-blending-modes
+//
 
 #import <UIKit/UIKit.h>
 
--- MaterialTextField/UIImage+MFTint.m
+++ MaterialTextField/UIImage+MFTint.m
@@ -5,6 +5,9 @@
 //  Created by Steph Sharp on 6/08/2015.
 //  Copyright (c) 2015 Stephanie Sharp. All rights reserved.
 //
+//  With thanks to Gordon Fontenot from thoughtbot:
+//  https://robots.thoughtbot.com/designing-for-ios-blending-modes
+//
 
 #import "UIImage+MFTint.h"
 
@@ -12,21 +15,11 @@
 
 - (UIImage *)mf_tintedImageWithColor:(UIColor *)tintColor
 {
-    return [self tintedImageWithColor:tintColor blendingMode:kCGBlendModeDestinationIn];
-}
-
-#pragma mark - Private methods
-
-- (UIImage *)tintedImageWithColor:(UIColor *)tintColor blendingMode:(CGBlendMode)blendMode
-{
     UIGraphicsBeginImageContextWithOptions(self.size, NO, 0.0f);
     [tintColor setFill];
     CGRect bounds = CGRectMake(0, 0, self.size.width, self.size.height);
     UIRectFill(bounds);
-    [self drawInRect:bounds blendMode:blendMode alpha:1.0f];
-
-    if (blendMode != kCGBlendModeDestinationIn)
-        [self drawInRect:bounds blendMode:kCGBlendModeDestinationIn alpha:1.0];
+    [self drawInRect:bounds blendMode:kCGBlendModeDestinationIn alpha:1.0f];
 
     UIImage *tintedImage = UIGraphicsGetImageFromCurrentImageContext();
     UIGraphicsEndImageContext();
