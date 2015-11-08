d615c74cbc5c4e4f5e57efb2946b1bd356fdbb04
Stephanie Sharp <ssharp@odecee.com.au>
2015-11-09 09:56:54 +1100

    Add code comments to document MFTextField properties

--- MaterialTextField/MFTextField.h
+++ MaterialTextField/MFTextField.h
@@ -11,44 +11,73 @@
 IB_DESIGNABLE
 @interface MFTextField : UITextField
 
+/**
+ * textPadding.height: Padding above and below the text field, between the placeholder label and underline.
+ * textPadding.width:  Horizontal padding is yet to be implemented.
+ */
 @property (nonatomic) IBInspectable CGSize textPadding;
 
+/**
+ * Animate placeholder label above text field on focus or input (see placeholderAnimatesOnFocus).
+ * Default is YES.
+ */
 @property (nonatomic) IBInspectable BOOL animatesPlaceholder;
 
 /**
- * Defaults to NO. If set to YES, placeholder will animate up on focus instead of on text input.
+ * Default is NO. If YES, the placeholder label will animate up on focus rather than on text input.
  */
 @property (nonatomic) IBInspectable BOOL placeholderAnimatesOnFocus;
 
 /**
- * Optional property to set the color of the textfield's default placeholder
+ * The color of the text field's default placeholder (displayed when text field is empty).
  */
 @property (nonatomic) IBInspectable UIColor *defaultPlaceholderColor;
 
+/**
+ * The color of the placeholder label when the text field is not in focus.
+ */
 @property (nonatomic) IBInspectable UIColor *placeholderColor;
 
 /**
  * Defaults to the first applicable font:
  * - the attributed placeholder font at the default placeholder size
- * - the textField font at the default placeholder size
+ * - the text field font at the default placeholder size
  */
 @property (nonatomic) UIFont *placeholderFont;
 
+/**
+ * The height of the underline when the text field is not in focus.
+ */
 @property (nonatomic) IBInspectable CGFloat underlineHeight;
 
+/**
+ * The height of the underline when the text field is in focus.
+ */
 @property (nonatomic) IBInspectable CGFloat underlineEditingHeight;
 
+/**
+ * The color of the underline when the text field is not in focus.
+ */
 @property (nonatomic) IBInspectable UIColor *underlineColor;
 
 /** 
- * The error message displayed under the text field is the NSError's localized description.
+ * To display an error under the text field, provide an NSError with a localized description.
  */
 @property (nonatomic) NSError *error;
 
+/**
+ * The font for the error displayed under the text field.
+ */
 @property (nonatomic) UIFont *errorFont;
 
+/**
+ * The color of the error displayed under the text field.
+ */
 @property (nonatomic) IBInspectable UIColor *errorColor;
 
+/**
+ * The vertical padding between the underline and the error label.
+ */
 @property (nonatomic) IBInspectable CGFloat errorPadding;
 
 @end
